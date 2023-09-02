c45b07815789306b0c89e9217fd4bf9fe30ade63
COllins <collinsmuiruri77@gmail.com>
2023-09-02 19:32:57 +0300

    Added error handling middleware

--- Server/routes/users/user.router.js
+++ Server/routes/users/user.router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { httpPostUsers } = require("./user.controller");
+const asyncHandler = require("express-async-handler");
 
 const usersRouter = express.Router();
 
-usersRouter.post("/user", httpPostUsers);
+usersRouter.post("/user", asyncHandler(httpPostUsers));
 
 module.exports = {
   usersRouter,
--- Server/server.js
+++ Server/server.js
@@ -2,20 +2,21 @@ const express = require("express");
 const { connectDatabase } = require("./config/mongoDb");
 const { usersRouter } = require("./routes/users/user.router");
 const { productsRouter } = require("./routes/products/product.router");
+const { notFound, errorHandler } = require("./Middleware/Error");
 
 require("dotenv").config();
 connectDatabase();
 const app = express();
 
 //API
-
 app.use("/api/import", usersRouter);
 app.use("/api/import", productsRouter);
 app.use("/api/products", productsRouter);
 
-app.get("/", (req, res) => {
-  res.send("Api is Running");
-});
+//ERROR HANDLERS
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 1000;
 
