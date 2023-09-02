1f2d8b92cbc4d6f2dc5eb8967c717bb89c638e1c
COllins <collinsmuiruri77@gmail.com>
2023-09-02 14:15:37 +0300

    Connected to mongoDB

---
+++ Server/models/products.mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const { reviewSchema } = require("./reviews.mongo");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
