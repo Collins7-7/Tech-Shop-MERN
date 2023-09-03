b788e6aebc02fb61bd8c0f77ab5af9aef8dce379
COllins <collinsmuiruri77@gmail.com>
2023-09-03 22:43:57 +0300

    Enables review of products

--- Server/routes/products/product.router.js
+++ Server/routes/products/product.router.js
@@ -3,14 +3,17 @@ const {
   httpPostProducts,
   httpGetProducts,
   httpGetSingleProduct,
+  httpPostReview,
 } = require("./product.controller");
 const asyncHandler = require("express-async-handler");
+const { protect } = require("../../Middleware/AuthMiddleware");
 
 const productsRouter = express.Router();
 
 productsRouter.post("/products", asyncHandler(httpPostProducts));
 productsRouter.get("/", asyncHandler(httpGetProducts));
 productsRouter.get("/:id", asyncHandler(httpGetSingleProduct));
+productsRouter.post("/:id/review", protect, asyncHandler(httpPostReview));
 
 module.exports = {
   productsRouter,
