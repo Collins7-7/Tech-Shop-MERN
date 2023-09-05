394230d76d8c92d3efd16f87256a60972cd2789a
COllins <collinsmuiruri77@gmail.com>
2023-09-05 17:12:06 +0300

    updated mongo url

--- server/config/mongoDb.js
+++ server/config/mongoDb.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-
+const MONGO_URL =
+  "mongodb+srv://admin:aA3VKuEXvlVZCc6h@tech-shop.qfwhvch.mongodb.net/techShopDB?retryWrites=true&w=majority";
 const connectDatabase = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL);
+    const connection = await mongoose.connect(MONGO_URL);
     console.log("Mongo connected");
   } catch (error) {
     console.log(`Error: ${error.message}`);
